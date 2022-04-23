f169224275b32bdb823fa462831850e969a906dd
LudoTexx <ludovic-teixeira@outlook.com>
2022-04-23 15:57:52 +0200

    23-04-2022

--- DeditecGPIOTest/DeditecGPIOTest/MainWindow.xaml.cs
+++ DeditecGPIOTest/DeditecGPIOTest/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using SharpDX;
 using SharpDX.DirectInput;
 using DeLib;
+using System.Threading;
 
 namespace DeditecGPIOTest
 {
@@ -73,9 +74,7 @@ namespace DeditecGPIOTest
                         else if(JoyRead > 50)
                         {
                             DataDeditec.pwm2 = JoyRead;
-
                         }
-
                     }
                 });
 
@@ -96,53 +95,108 @@ namespace DeditecGPIOTest
                     Console.WriteLine("Could not open module!");
                     IsError();
                     Console.WriteLine("Press any key to exit");
-                    Console.ReadKey();
+                    //Console.ReadKey();
                     Environment.Exit(0);
                 }
 
-                Task.Run(() => 
-                { 
-                    while(true)
+                DeLibNET.DapiSpecialCommand(handle,DeLibNET.DAPI_SPECIAL_CMD_TIMEOUT, DeLibNET.DAPI_SPECIAL_TIMEOUT_DEACTIVATE, 0, 0);
+
+                Task.Run(() =>
+                {
+                    try
                     {
-                        if(joyStat.Buttons[11])
+                        while (true)
                         {
-                           DeLibNET.DapiDOSet1(handle, 5, 1);
+
+                            if (joyStat.Buttons[11])
+                            {
+                                DeLibNET.DapiDOSet1(handle, 5, 1);
+                                DeLibNET.DapiDOSet1(handle, 6, 1);
+                                DeLibNET.DapiDOSet1(handle, 7, 1);
+                                DeLibNET.DapiDOSet1(handle, 8, 1);
+                                DeLibNET.DapiDOSet1(handle, 9, 1);
+                                DeLibNET.DapiDOSet1(handle, 10, 1);
+                                DeLibNET.DapiDOSet1(handle, 11, 1);
+                                DeLibNET.DapiDOSet1(handle, 12, 1);
+                                DeLibNET.DapiDOSet1(handle, 13, 1);
+                                DeLibNET.DapiDOSet1(handle, 14, 1);
+                                DeLibNET.DapiDOSet1(handle, 15, 1);
+                            }
+                            else
+                            {
+                                DeLibNET.DapiDOSet1(handle, 5, 0);
+                                DeLibNET.DapiDOSet1(handle, 6, 0);
+                                DeLibNET.DapiDOSet1(handle, 7, 0);
+                                DeLibNET.DapiDOSet1(handle, 8, 0);
+                                DeLibNET.DapiDOSet1(handle, 9, 0);
+                                DeLibNET.DapiDOSet1(handle, 10, 0);
+                                DeLibNET.DapiDOSet1(handle, 11, 0);
+                                DeLibNET.DapiDOSet1(handle, 12, 0);
+                                DeLibNET.DapiDOSet1(handle, 13, 0);
+                                DeLibNET.DapiDOSet1(handle, 14, 0);
+                                DeLibNET.DapiDOSet1(handle, 15, 0);
+                            }
+
+                            if(JoyRead > 50 && JoyRead < 51)
+                            {
+                                for(uint i = 0; i < 15; i++)
+                                {
+                                    DeLibNET.DapiPWMOutSet(handle, i, 0);
+                                }
+                            }
+
+                            if (JoyRead < 49.99)
+                            {
+                                float final = 0 + (JoyRead - 50) * (100 - 0) / (0 - 50);
+                                DeLibNET.DapiPWMOutSet(handle, 0, final);
+                                DeLibNET.DapiPWMOutSet(handle, 1, 0);
+
+                                Console.WriteLine("Write :" +final + "   READ :" + DeLibNET.DapiPWMOutReadback(handle, 0));
+
+                                DeLibNET.DapiDOSet1(handle, 3, 1);
+                                DeLibNET.DapiDOSet1(handle, 4, 0);
+                            }
+                            else if (JoyRead > 50.99)
+                            {
+                                float final = 0 + (JoyRead - 50) * (100 - 0) / (100 - 50);
+                                DeLibNET.DapiPWMOutSet(handle, 1, final);
+                                DeLibNET.DapiPWMOutSet(handle, 0, 0);
+
+                                Console.WriteLine("Write :" + final + "   READ :" + DeLibNET.DapiPWMOutReadback(handle, 1));
+
+                                DeLibNET.DapiPWMOutSet(handle, 2, final);
+                                DeLibNET.DapiPWMOutSet(handle, 3, final);
+                                DeLibNET.DapiPWMOutSet(handle, 4, final);
+                                DeLibNET.DapiPWMOutSet(handle, 5, final);
+                                DeLibNET.DapiPWMOutSet(handle, 6, final);
+                                DeLibNET.DapiPWMOutSet(handle, 7, final);
+                                DeLibNET.DapiPWMOutSet(handle, 8, final);
+                                DeLibNET.DapiPWMOutSet(handle, 9, final);
+                                DeLibNET.DapiPWMOutSet(handle, 10, final);
+                                DeLibNET.DapiPWMOutSet(handle, 11, final);
+                                DeLibNET.DapiPWMOutSet(handle, 12, final);
+                                DeLibNET.DapiPWMOutSet(handle, 13, final);
+                                DeLibNET.DapiPWMOutSet(handle, 14, final);
+                                DeLibNET.DapiPWMOutSet(handle, 15, final);
+                                DeLibNET.DapiDOSet1(handle, 3, 0);
+                                DeLibNET.DapiDOSet1(handle, 4, 1);
+                            }
+
+                            DataDeditec.analog1 = 0 + (DeLibNET.DapiADGetVolt(handle, 0) - 0) * (100 - 0) / (5 - 0);
+                            DataDeditec.analog2 = 0 + (DeLibNET.DapiADGetVolt(handle, 1) - 0) * (100 - 0) / (5 - 0);
+
+                            DataDeditec.digital1 = DeLibNET.DapiADGetVolt(handle, 4) < 2;
+                            DataDeditec.digital2 = DeLibNET.DapiADGetVolt(handle, 2) < 2;
+                            DataDeditec.digital3 = DeLibNET.DapiADGetVolt(handle, 3) < 2;
+
+                            Thread.Sleep(10);
+                            //Task.Delay(10000);
                         }
-                        if (!joyStat.Buttons[11])
-                        {
-                            DeLibNET.DapiDOSet1(handle, 5, 0);
-                        }
-
-                        if (JoyRead < 50)
-                        {
-                            float final = 0 + (JoyRead - 50) * (100 - 0) / (0 - 50);
-                            DeLibNET.DapiPWMOutSet(handle, 0, final);
-                            DeLibNET.DapiDOSet1(handle, 3, 1);
-                            DeLibNET.DapiDOSet1(handle, 4, 0);
-                        }
-                        else if (JoyRead > 50)
-                        {
-                            float final = 0 + (JoyRead - 50) * (100 - 0) / (100 - 50);
-                            DeLibNET.DapiPWMOutSet(handle, 1, final);
-                            DeLibNET.DapiDOSet1(handle, 3, 0);
-                            DeLibNET.DapiDOSet1(handle, 4, 1);
-                        }
-
-                        DataDeditec.analog1 = 0 + (DeLibNET.DapiADGetVolt(handle, 0) - 0) * (100 - 0) / (5 - 0) ;
-                        DataDeditec.analog2 = 0 + (DeLibNET.DapiADGetVolt(handle, 1) - 0) * (100 - 0) / (5 - 0);
-
-
-                        DataDeditec.digital1 = DeLibNET.DapiADGetVolt(handle, 4) < 2;
-                        DataDeditec.digital2 = DeLibNET.DapiADGetVolt(handle, 2) < 2;
-                        DataDeditec.digital3 = DeLibNET.DapiADGetVolt(handle, 3) < 2;
-
-
-                        Task.Delay(100);
                     }
+                    catch (Exception ex) { Console.WriteLine(ex.ToString()); }
                 });
-                
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString());
             }
@@ -164,7 +218,7 @@ namespace DeditecGPIOTest
                 DeLibNET.DapiGetLastErrorText(msg, 256);    //msg.Capacity
                 Console.WriteLine("Error Code = {0}, Message = {1}", Convert.ToString(error), msg);
                 Console.WriteLine("Press any key to exit");
-                Console.ReadKey();
+                //Console.ReadKey();
                 Environment.Exit(0);
             }
         }
